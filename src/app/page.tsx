1eca0c32e72524ee633370e3f15184450e422cd6
Josh Pham <JTPham911@gmail.com>
2024-07-10 14:37:57 -0700

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to Josh Pham{`'`}s website.</h1>
+    <main className="flex min-h-screen flex-col items-center p-24 gap-2">
+      <h1 className="text-2xl font-bold">Welcome to Josh Pham{`'`}s website.</h1>
+      <p>I have decent skill with React and Django</p>
+      <hr />
+      <div className="flex flex-col space-x-4 mt-3">
+        <h1>Contact Info</h1>
+        <ul>
+          <li>Email: JTPham911@gmail.com</li>
+        </ul>
+      </div> 
     </main>
   );
 }
