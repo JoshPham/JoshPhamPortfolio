f0e2b11f40435cbfcf17c50e9fa05fe45a8ef338
Josh Pham <JTPham911@gmail.com>
2024-07-10 16:40:01 -0700

    added learn page

---
+++ src/app/learn/page.tsx
@@ -0,0 +1,14 @@
+
+
+const LearnPage = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24 gap-2">
+            <div className="flex flex-col justify-center items-center gap-3">
+            <h1 className="text-3xl font-bold">Learn</h1>
+            <p>I'm planning on adding a learning page, so stay tuned</p>
+            </div>
+        </main>
+    );
+};
+
+export default LearnPage;
