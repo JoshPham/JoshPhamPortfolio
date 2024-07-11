d65c2ede9df2dd9ed610895592a2b70b546b84ab
Josh Pham <JTPham911@gmail.com>
2024-07-10 20:17:29 -0700

    changed a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-2">
-      <h1 className="text-2xl font-bold">Welcome to Josh Pham{`'`}s website.</h1>
+    <main className="flex min-h-screen flex-col items-center p-24 gap-6">
+      <h1 className="text-4xl font-bold">Welcome to Josh Pham{`'`}s website.</h1>
       <p>I have decent skill with React and Django</p>
-      <hr />
-      <div className="flex flex-col space-x-4 mt-3">
-        <h1>Contact Info</h1>
+      <div className="flex flex-col space-x-4 mt-3 border-2 p-5">
+        <h1 className="font-bold">Contact Info</h1>
         <ul>
           <li>Email: JTPham911@gmail.com</li>
         </ul>
