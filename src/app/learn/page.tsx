717d5002b9ae57f454c4b6eba4ee639ce965b6b6
Josh Pham <JTPham911@gmail.com>
2024-07-10 20:40:08 -0700

    changed a quote mark

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -5,7 +5,7 @@ const LearnPage = () => {
         <main className="flex min-h-screen flex-col items-center p-24 gap-2">
             <div className="flex flex-col justify-center items-center gap-3">
             <h1 className="text-3xl font-bold">Learn</h1>
-            <p>I'm planning on adding a learning page, so stay tuned</p>
+            <p>I{`'`}m planning on adding a learning page, so stay tuned</p>
             </div>
         </main>
     );
