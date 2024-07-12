1eb453ec584d71007dfbefeed438306c9677f066
Josh Pham <JTPham911@gmail.com>
2024-07-11 23:22:55 -0700

    squash commit

--- src/app/(navigation)/layout.tsx
+++ src/app/(navigation)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({children}: {children: React.ReactNode}) {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 w-full h-16 border-b-2 border-gray-700">
+      <nav className="fixed top-0 left-0 w-full h-16 border-b-2 border-gray-700 bg-black">
         <ul className="flex justify-end items-center w-full h-full px-2">
           <li>
               <a href={learnLink} className="z-50 text-xl bg-emerald-500 hover:bg-emerald-400 px-4 py-2 font-bold rounded-full drop-shadow-2xl hover:drop-shadow-3xl hover:scale-105 inline-block transition-all duration-150 ease-in">
--- src/app/(navigation)/page.tsx
+++ src/app/(navigation)/page.tsx
@@ -1,46 +1,74 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
+
+import Image from 'next/image';
+
+import clipboard from '../../images/clipboard.png';
+import checkmark from '@/images/checkmark.png';
+
+import styles from './mainpage.module.css';
 
 export default function Home() {
+  const [copiedEmail, setCopiedEmail] = useState(false);
+
   const copyToClipboard = async (e: React.MouseEvent<HTMLButtonElement | HTMLParagraphElement>) => {
     const target = e.target as HTMLButtonElement | HTMLParagraphElement;
     const email = "JTPham911@gmail.com";
     try {
       await navigator.clipboard.writeText(email);
 
-      // Find the copy button
-      const copyButton = document.getElementById('copyButton') as HTMLButtonElement;
-      copyButton.innerHTML = "Copied!";
-      copyButton.classList.add('font-extrabold');
+      setCopiedEmail(true);
 
       setTimeout(() => {
-        copyButton.innerHTML = "Copy";
-        copyButton.classList.remove('font-extrabold');
-      }, 2000);
+        setCopiedEmail(false);
+      }, 2500);
     } catch (err) {
       console.error('Failed to copy text: ', err);
     }
   };
 
   return (
-    <main className="flex flex-col items-center p-24 gap-6">
+    <main className="flex flex-col items-center p-24 gap-12">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#00422e_100%)] bottom-0"></div>
       <h1 className="font-bold text-4xl">Welcome to Josh Pham{`'`}s website.</h1>
-      <p>I have decent skill with React and Django</p>
+      <div className="flex flex-col gap-2 w-50"> 
+        <h1 className="font-bold text-xl">Skills</h1>
+        <ul className="flex flex-wrap text-lg gap-3 font-mono">
+          <li>React</li>
+          <li>Next.js</li>
+          <li>Django</li>
+          <li>Python</li>
+          <li>Java</li>
+        </ul>
+      </div>
       <div className="relative group">
-        <div className="absolute inset-0.5 bg-gradient-to-tr from-emerald-600 to-emerald-300 blur group-hover:blur-md group-hover:-inset-0.5 transition-all duration-1000 group-hover:duration-200"></div>
-        <div className="relative bg-black flex flex-col space-x-4 border-2 p-5 transition-all ease h-full">
+        <div className="absolute inset-0 bg-gradient-to-tr from-emerald-600 to-emerald-300 blur group-hover:blur-md group-hover:-inset-0.5 transition-all duration-1000 group-hover:duration-200 w-full"></div>
+        <div className="relative bg-black flex flex-col space-x-4 border-2 p-5 transition-all ease h-full w-full xxs:pe-9 xs:pe-5">
           <h1 className="font-bold">Contact Info</h1>
           <ul className="">
-            <li className="flex w-full gap-1 group">
+            <li className="flex w-full gap-2 group">
               Email:
               <p onClick={copyToClipboard} className="cursor-pointer group-hover:underline">JTPham911@gmail.com</p>
-              <button id="copyButton" onClick={copyToClipboard} className='font-semibold'>Copy</button>
+              <Image
+                src={clipboard}
+                alt="clipboard"
+                width={25}
+                layout="fixed"
+                className="cursor-pointer inline-block"
+                onClick={copyToClipboard}
+              />
             </li>
           </ul>
         </div> 
       </div>
+      
+      {copiedEmail && (
+        <div className="absolute flex items-center gap-2 left-8 bottom-8 bg-emerald-500 px-4 py-3 rounded-md fade-in-out opacity-0">
+          <h1>Copied email to clipboard!</h1>
+        </div>
+      )}
+
     </main>
   );
 }
